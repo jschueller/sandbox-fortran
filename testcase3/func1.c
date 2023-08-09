b2e14e9790de2eea924be7453ea63eee6034bb4b
Julien Schueller <schueller@phimeca.com>
2023-08-09 22:20:14 +0200

    cloc

--- testcase3/func1.c
+++ testcase3/func1.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-void func1(double *a, double *b)
+void func1(double *a, double *b, double *c)
 {
   printf("func1 (c)\n");
   printf("func1 (c) a=%x\n", (unsigned)a);
   printf("func1 (c) a0=%g\n", a[0]);
   printf("func1 (c) b=%x\n", (unsigned)b);
   b[0] = 7.;
+  printf("func1 (c) c=%x\n", (unsigned)c);
 }
--- testcase3/main1.f90
+++ testcase3/main1.f90
@@ -1,23 +1,25 @@
 program prog1
   use iso_c_binding, only : C_DOUBLE, C_INT
   interface
-    subroutine func1(a, b) BIND(C)
+    subroutine func1(a, b, c) BIND(C)
     use iso_c_binding, only : C_DOUBLE, C_INT
     implicit none
     real(kind=C_DOUBLE), intent(in) :: a(*)
-    real(kind=C_DOUBLE), intent(out) :: b(*)
+    real(kind=C_DOUBLE), intent(out) :: b
+    real(kind=C_DOUBLE), intent(out) :: c(*)
     end subroutine func1
   end interface
 
   real(kind=C_DOUBLE) :: a_loc(2)
-  real(kind=C_DOUBLE) :: b_loc(3)
-
+  real(kind=C_DOUBLE) :: b_loc
+  real(kind=C_DOUBLE) :: c_loc(0)
   a_loc = 5.
   b_loc = 6.
   print *, 'loc(a)=', loc(a_loc)
   print *, 'loc(b)=', loc(b_loc)
+  print *, 'loc(c)=', loc(c_loc)
   print *, 'calling func1...'
-  call func1(a_loc, b_loc)
-  print *, 'func1 ok b=', b_loc(1)
+  call func1(a_loc, b_loc, c_loc)
+  print *, 'func1 ok b=', b_loc
 end program prog1
 
